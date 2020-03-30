2b9e6483c71e92f93d46ceea47fb21b32773d265
wllxxxy <wllxxxy@126.com>
2020-03-30 22:32:21 +0800

    修改wllsdk

---
+++ src/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wll
+ * Date: 2020/3/30
+ * Time: 22:27
+ */
+
+namespace wllxxxy\wllsdk;
+
+
+class Test
+{
+    public function test()
+    {
+        echo 'test';
+    }
+}
---
+++ tests/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wll
+ * Date: 2020/3/30
+ * Time: 22:28
+ */
+
+require '../vendor/autoload.php';
+
+$test = new \wllxxxy\wllsdk\Test();
+$test->test();// echo 'test';
